b6c3225cfbb5fd4bb565996daafaf6e8570f1ed7
Andrew Sorem <andrewsorem9@gmail.com>
2024-08-21 16:08:25 -0700

    Main.java

--- Main.java
+++ Main.java
@@ -12,47 +12,7 @@ public class Main {
         return GPA;
     }
 
-    CPU Ryzen1 = new CPU("AMD", "7950X3D", 4.2);
+    CPU ryzen1 = new CPU("AMD", "7950X3D", 4.2);
     System.out.println(CPU.toString());
-    
-}
-
-public class CPU {
-
-    //instance variables
-    private String company;
-    private String cpuName;
-    private double speed;
-
-    //getters
-    public String getCompany(){
-      return company;
-    }
-    public String getCPUName(){
-      return cpuName;
-    }
-    public double getSpeed(){
-      return speed;
-    }
-
-    //setters
-    public void setCompany(String company) {
-        this.company = company;
-    }
-    public void setCPUName(String cpuName) {
-        this.cpuName = cpuName;
-    }
-    public void setSpeed(double speed) {
-        this.speed = speed;
-    }
-
-    //other methods
-    public String toString() {
-      return  String.format("Company: %s\nCPU Name: %s\nCPU Speed: %0.2f", company, cpuName, speed);
-    }
-
-    public boolean equals(CPU other) {
-      return (company == other.company && cpuName == other.cpuName && speed == other.speed);
-    }
     
 }
